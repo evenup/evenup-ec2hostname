408e3e1b0670c7051a964a591d5ecd42e25b5991
Justin Lambert <jlambert@eml.cc>
2014-12-12 13:01:16 -0700

    normalize common files

--- spec/classes/ec2hostname_install_spec.rb
+++ spec/classes/ec2hostname_install_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
 describe 'ec2hostname::install', :type => :class do
-  let(:params) { {
-    :aws_key     => 'abc',
-    :aws_secret  => 'def',
-    :hostname    => 'myhost',
-    :domain      => 'mydomain.com',
-    :ttl         => 60,
-    :type        => 'CNAME',
-    :target      => 'local-hostname',
-    :zone        => '123',
-  } }
+  let(:pre_condition) { 'class { "ec2hostname":
+    aws_key     => "abc",
+    aws_secret  => "def",
+    hostname    => "myhost",
+    domain      => "mydomain.com",
+    ttl         => 60,
+    type        => "CNAME",
+    target      => "local-hostname",
+    zone        => "123",
+    }'
+  }
 
   context 'aws-sdk gem' do
     context 'not install by default' do
@@ -18,7 +19,18 @@ describe 'ec2hostname::install', :type => :class do
     end
 
     context 'install with install_gem' do
-      let(:pre_condition) { [ "class ec2hostname { $install_gem = true }", "include ec2hostname" ] }
+      let(:pre_condition) { 'class { "ec2hostname":
+        aws_key     => "abc",
+        aws_secret  => "def",
+        hostname    => "myhost",
+        domain      => "mydomain.com",
+        ttl         => 60,
+        type        => "CNAME",
+        target      => "local-hostname",
+        zone        => "123",
+        install_gem => true,
+        }'
+      }
       it { should contain_package('aws-sdk').with(:provider => 'gem') }
     end
   end
